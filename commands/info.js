79baee29cb99a1ccbf6e80d0be16867db3e11777
Leo MG Nesfield (LMGN) <contact@thelmgn.com>
2018-01-14 14:07:02 +0000

    :lipstick: Floor the ping.

--- commands/info.js
+++ commands/info.js
@@ -6,7 +6,7 @@ exports.run = async(Client,Message,Arguments) => {
     return Message.reply({embed:new Discord.RichEmbed()
                         .setTitle("Botstion³")
                         .setDescription(`Made by theLMGN in 2017 with :heart: using Node<:js:388353565619519488>.\n\nThanks to [SunburntRock89](http://sr89.ml) for some help and also [Sam's Cookie](http://github.com/samfromyt/cookie) which I stole some code from`)
-                        .addField(":ping_pong: Ping", Client.ping + "ms",true) 
+                        .addField(":ping_pong: Ping", Math.floor(Client.ping) + "ms",true) 
                         .addField("<:js:388353565619519488> Node Version", process.version,true) 
                         .addField("<:Discord:375377712681844736> Discord.JS Version", Discord.version,true) 
                         .addField(":clock10: Uptime (hours)", Math.floor(Client.uptime / 60 / 60 / 60),true)
