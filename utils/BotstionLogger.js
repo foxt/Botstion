cb14b8ffc4b148d1388fc5a8745b5bbdb19822da
Leo MG Nesfield (LMGN) <contact@thelmgn.com>
2018-01-26 19:09:31 +0000

    :speaker: Basic logging (requires a debugger)

--- utils/BotstionLogger.js
+++ utils/BotstionLogger.js
@@ -5,15 +5,24 @@ const prefix = {
     crit: "\x1b[0m\x1b[41m\x1b[37m\x1b[1m\x1b[5m\x1b[4m\x1b[31m CRIT: "
 }
 
+let logmessages = []
+
 exports.err = function(msg) {
-    console.error(prefix.err + msg)
+    logmessages.push({type: "error", message: msg})
+    console.error(prefix.err,msg)
 }
 exports.warn = function(msg) {
-    console.warn(prefix.warn + msg)
+    logmessages.push({type: "warn", message: msg})
+    console.warn(prefix.warn, msg)
 }
 exports.info = function(msg) {
-    console.info(prefix.info + msg)
+    logmessages.push({type: "info", message: msg})
+    console.info(prefix.info, msg)
 }
 exports.crit = function(msg) {
-    console.error(prefix.crit + msg)
+    logmessages.push({type: "crit", message: msg})
+    console.error(prefix.crit, msg)
+}
+exports.getLogs = function() {
+    return logmessages
 }
