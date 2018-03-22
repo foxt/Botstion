41ea21ee659e2eb5c90909c7547c86d63574edfc
Leo Nesfield <git@thelmgn.com>
2018-03-22 09:50:26 +0000

    :bug: Improve eval

--- plugins/eval.js
+++ plugins/eval.js
@@ -10,12 +10,20 @@ module.exports = {
 		usage: "console.log(\"Hello World!\")",
 		description: "Executes some code.",
 		execute: async(c, m, a) => {
-			try {
-				let result = eval(a.join(" "));
-				result = result.replace(config.token, "https://i.imgur.com/J7sAzzC.png");
-				return m.reply(result);
-			} catch (err) {
-				return m.reply(`Woops, we had an error.\n\`\`\`${err}\`\`\``);
+			if (config.maintainers.includes(m.author.id)) {
+				try {
+					let result = eval(a.join(" ").replace("c.token", "").replace("client.token", "")
+						.replace("[\"token\"]", ""));
+					result = result.replace(config.token, "https://i.imgur.com/J7sAzzC.png");
+					return m.reply(result);
+				} catch (err) {
+					return m.reply(`Woops, we had an error.\n\`\`\`${err}\`\`\``);
+				}
+			} else {
+				m.reply({ embed: new Discord.MessageEmbed()
+					.setAuthor("401: Access denied.", "https://cdn.discordapp.com/attachments/423185454582464512/425761155940745239/emote.png")
+					.setColor("#ff3860")
+					.setFooter(`You do not have permissions to run this command. Sorry.`) });
 			}
 		},
 	}],
