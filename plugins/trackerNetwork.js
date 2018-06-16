f637670802ba34c4fa66926b1d43b76c0d69b37e
Leo MG Nesfield (LMGN) <contact@thelmgn.com>
2018-06-17 00:13:13 +0100

    :hankey: Create a file for TNN

---
+++ plugins/trackerNetwork.js
@@ -0,0 +1,11 @@
+module.exports = {
+	name: "TrackerNetwork",
+	author: "theLMGN",
+	version: 1,
+	description: "Game stats provided by thetrackernetwork.com",
+	commands: [],
+	events: [],
+	timer: [],
+};
+
+//https://api.fortnitetracker.com/v1/profile/{platform}/{epic-nickname}
