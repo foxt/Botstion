309a2fc8ff61e8cce4d5dd3a28aedbc90c597ec4
Leo Nesfield <contact@thelmgn.com>
2018-11-07 16:35:08 +0000

    bork

---
+++ plugins/bork.js
@@ -0,0 +1,17 @@
+module.exports = {
+	name: "bork",
+	author: "theLMGN",
+	version: 1,
+	description: "Bork",
+	commands: [
+		{
+			name: "<@321746347550310411>",
+			usage: "",
+			description: "Bork",
+			execute: async(c, m, a) => {
+				m.reply("bork")
+		},
+	],
+	events: [],
+	timer: [],
+};
