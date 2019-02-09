df37e28d92e68041bcd0fb693a295a6e0b67006d
Leo Nesfield <leo@thelmgn.com>
2019-02-09 22:16:33 +0000

    :bug: show prefix in playing message

--- plugins/game.js
+++ plugins/game.js
@@ -6,6 +6,6 @@ module.exports = {
 	commands: [],
 	events: [],
 	timer: [async c => {
-		c.user.setPresence({ activity: { name: `in ${c.guilds.array().length} guilds | b!help` }, status: "online" });
+		c.user.setPresence({ activity: { name: `in ${c.guilds.array().length} guilds | ${require("../config/config.json").defaultPrefix}help` }, status: "online" });
 	}],
 };
