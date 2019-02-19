d11016b257030709e6cd2bd19056f4a78847c978
Leo Nesfield <leo@thelmgn.com>
2019-02-19 00:09:30 +0000

    :construction: Create shortcut

---
+++ db/plugin.js
@@ -0,0 +1,15 @@
+module.exports = {
+	name: "Database plugin",
+	author: "theLMGN",
+	version: 1,
+	description: "This is the main plugin for handling database support in Botstion.",
+	commands: [],
+	events: [],
+	timer: [],
+	addons: {
+		db: {
+
+
+		}
+	}
+};
