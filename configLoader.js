c55ce3df71f46f9913c75f9a2ffa16be6c154dab
Ben <bennixerus@gmail.com>
2019-03-05 21:36:04 +0000

    Put config loader in own class
    So that this monster of a thing doesn't have to be inside each file

---
+++ configLoader.js
@@ -0,0 +1,20 @@
+let config = []
+if(fs.existsSync("./config/config.js")) {
+	config = require("./config/config.js")
+} else if(fs.existsSync("./config/defaultConfig.js")){
+	config = require("./config/defaultConfig.js")
+	console.log("Botstion has fallen back to the default config file!")
+} else {
+	console.log("No configurations found (tried defaultConfig.js and config.js), terminating!")
+	process.exit(1);
+}
+
+for(var key in config){
+	console.log(process.env)
+	if(key in process.env){
+		config[key] = process.env[key]
+		console.log("Found " + key + " from environment variable - overriding config!")
+	}
+}
+
+module.exports = config
