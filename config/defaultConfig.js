32f8b2b676188f4135cadb841fad186dedcca29e
Leo Nesfield <leo@thelmgn.com>
2019-11-16 14:35:19 +0000

    :sparkles: Currency conversion

--- config/defaultConfig.js
+++ config/defaultConfig.js
@@ -1,24 +1,26 @@
 // Rename me to config.js and fill it in.
+// Not all API keys are required (plugins that require an API key that isn't provided will not load.)
 
 module.exports = {
-	token: "",
-	defaultPrefix: "b!",
-	maintainers: ["158311402677731328"],
-	errorReportChannel: "600082294258139137",
-	geniusAccessToken: "",
+	token: "discordapp.com/developers/", // discord token
+	defaultPrefix: "b!", // bot's prefix
+	maintainers: ["158311402677731328"], // allowed access to eval, shutdown etc command
+	errorReportChannel: "600082294258139137", // exceptions will be sent to this channel
+	geniusAccessToken: "https://genius.com/developers", // b!lyrics
 	dblToken:"", // top.gg token, same token is used as the voting authorization
-	trackerNetworkApiKey: "",
-	ipinfoioToken: "",
-	googleApi: "",
-	imgurClientID: "",
-	darkskyApiKey: "",
+	trackerNetworkApiKey: "https://tracker.gg/developers", // used for fortnite, csgo, division, apex, etc
+	ipinfoioToken: "https://ipinfo.io/",
+	googleApi: "", // Used for b!ip maps, turn on the maps API
+	imgurClientID: "https://api.imgur.com/oauth2/addclient", // required for b!shortcuts
+	darkskyApiKey: "https://darksky.net/dev", //required for b!weather
+	alphaVantageKey: "https://www.alphavantage.co/support/#api-key", // used for b!currency and b!stocks
 	sequelize: [ // these are the arguments passed to the Sequelize init, see http://docs.sequelizejs.com/class/lib/sequelize.js~Sequelize.html
-		"botstion","username","password", {
+		"botstion","username","password", { // username and password can be left blank if using sqlite
 			host: "localhost",
 			dialect: "sqlite",
 			logging: false,
 			storage: "database.sqlite"
 		}
     ],
-    httpPort: 6597
+    httpPort: 6597 // port used for the built in http server
 }
---
+++ plugins/apis/alphavantage.js
@@ -0,0 +1,107 @@
+const Discord = require("discord.js");
+const config = require("../../configLoader");
+const fetch = require("node-fetch")
+let requestsRemaining = 5;
+let uptimeAtLastReset = process.uptime();
+
+var currencyListDownloaded = false
+var currencies = []
+
+function parseCurrencyList(text) {
+    var split = text.split("\n")
+    split.shift()
+    for (var currency of split) {
+        currencies.push(currency.split(",")[0])
+    }
+}
+
+module.exports = {
+	name: "Alpha Vantage",
+	author: "theLMGN",
+	version: 1,
+	description: "Currency conversion and stock exchange commands.",
+	requiresConfig: "alphaVantageKey",
+	commands: [
+		{
+			name: "currency",
+			usage: "50 GBP BTC",
+			description: "Currency conversions, {amount} {source currency} {to currency}",
+			execute: async(c, m, a) => {
+                if (!currencyListDownloaded) {
+                    return m.reply({ embed: new Discord.MessageEmbed()
+                        .setAuthor("428: Currency list not loaded.", "https://cdn.discordapp.com/attachments/423185454582464512/425761155940745239/emote.png")
+                        .setColor("#ff3860")
+                        .setFooter(`Please try again later.`) });
+                }
+				if (requestsRemaining < 1) {
+                    return m.reply({ embed: new Discord.MessageEmbed()
+                        .setAuthor("429: Ratelimited!", "https://cdn.discordapp.com/attachments/423185454582464512/425761155940745239/emote.png")
+                        .setColor("#ff3860")
+                        .setFooter(`Try again in ${60 - Math.floor(process.uptime() - uptimeAtLastReset)} seconds.`) });
+                }
+                if (a.length > 3) {
+                    return m.reply({ embed: new Discord.MessageEmbed()
+                        .setAuthor("400: Too many arguments", "https://cdn.discordapp.com/attachments/423185454582464512/425761155940745239/emote.png")
+                        .setColor("#ff3860")
+                        .setFooter(`This command only accepts 3 arguments, {amount} {source currency} {to currency}`) });
+                }
+                if (a.length < 3) {
+                    return m.reply({ embed: new Discord.MessageEmbed()
+                        .setAuthor("400: Too few arguments", "https://cdn.discordapp.com/attachments/423185454582464512/425761155940745239/emote.png")
+                        .setColor("#ff3860")
+                        .setFooter(`This command only accepts 3 arguments, {amount} {source currency} {to currency}`) });
+                }
+                var floatNumber = parseFloat(a[0])
+                if (isNaN(floatNumber)) {
+                    return m.reply({ embed: new Discord.MessageEmbed()
+                        .setAuthor("400: First argument is not a number.", "https://cdn.discordapp.com/attachments/423185454582464512/425761155940745239/emote.png")
+                        .setColor("#ff3860")
+                        .setFooter(`This command only accepts 3 arguments, {amount} {source currency} {to currency}`) });
+                }
+                if (currencies.includes(a[1].toLowerCase())) {
+                    return m.reply({ embed: new Discord.MessageEmbed()
+                        .setAuthor(`400: ${a[1]} is not a recognised currency.`, "https://cdn.discordapp.com/attachments/423185454582464512/425761155940745239/emote.png")
+                        .setColor("#ff3860")
+                        .setFooter(`This command only accepts 3 arguments, {amount} {source currency} {to currency}`) });
+                }
+                if (currencies.includes(a[2].toLowerCase())) {
+                    return m.reply({ embed: new Discord.MessageEmbed()
+                        .setAuthor(`400: ${a[2]} is not a recognised currency.`, "https://cdn.discordapp.com/attachments/423185454582464512/425761155940745239/emote.png")
+                        .setColor("#ff3860")
+                        .setFooter(`This command only accepts 3 arguments, {amount} {source currency} {to currency}`) });
+                }
+                requestsRemaining -= 1
+                var ftch = await fetch(`https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=${a[1]}&to_currency=${a[2]}&apikey=${config.alphaVantageKey}`)
+                var j = await ftch.json()
+                var exchange = j["Realtime Currency Exchange Rate"]
+                var fromName = exchange["2. From_Currency Name"]
+                var toName = exchange["4. To_Currency Name"]
+                var rate = parseFloat(exchange["5. Exchange Rate"])
+                return m.reply({ embed: new Discord.MessageEmbed()
+                    .setTitle(`${floatNumber} ${fromName} = ${floatNumber * rate} ${toName}`)
+                    .setColor("#3273dc")
+                    .setFooter(`Currency conversion powered by alphavantage.io`) });
+			}
+		}
+	],
+	events: [
+		{
+			name: "ready",
+			exec: async function(c) {
+				setInterval(function() {
+					requestsRemaining = 5;
+					uptimeAtLastReset = process.uptime();
+                },60000)
+                console.log("[AlphaVantage] Downloading list of real currencies")
+                var realFetch = await fetch("https://www.alphavantage.co/physical_currency_list/")
+                parseCurrencyList(await realFetch.text())
+
+                console.log("[AlphaVantage] Downloading list of virtual currencies")
+                var digitalFetch = await fetch("https://www.alphavantage.co/digital_currency_list/")
+                parseCurrencyList(await digitalFetch.text())
+                currencyListDownloaded = true
+                console.log(`[AlphaVantage] ${currencies.length} currencies loaded.`)
+			}
+		}
+	]
+};
