d6010980e05c6611021f49e82550b890d4b04103
Leo Nesfield <leo@thelmgn.com>
2020-04-05 20:19:01 +0100

    :sparkles: enable more agressive gc if --expose-gc is passed to node

---
+++ plugins/util/gc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    name: "Strict Garbage Collection",
+    author: "theLMGN",
+    disable: typeof gc == "undefined",
+    version: 1,
+    description: "Makes sure the grabage collector runs at least every time timer() is run.",
+    timer: [async c => {
+        gc(true)
+    }],
+};
+
