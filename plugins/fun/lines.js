da60cee3cfe94dac5253133ec7f4fb0e614c13c8
Leo Nesfield <leo@thelmgn.com>
2020-05-27 04:48:47 +0100

    :sparkles: make lines better

--- plugins/fun/lines.js
+++ plugins/fun/lines.js
@@ -1,26 +1,30 @@
 const Discord = require("discord.js");
 const fs = require("fs");
 
-var files = 0;
-
 function thing(folder) {
 	if (fs.lstatSync(folder).isDirectory()) {
 		if (!folder.includes("node_modules") && !folder.includes("config") && !folder.includes(".DS_Store") && !folder.includes(".github") && !folder.includes(".vscode") && !folder.includes(".git")) {
 			var lines = 0;
+			var chars = 0;
+			var files = 0;
 			for (var folderr of fs.readdirSync(folder)) {
-				lines = lines + thing(folder + "/" + folderr)
+				var t = thing(folder + "/" + folderr)
+				lines += t[0]
+				chars += t[1]
+				files += t[2]
 			}
-			return lines
+			return [lines,chars,files]
 		}
 	} else {
 		if (folder.includes(".") && !folder.endsWith(".json")) { // remove files that have no extension
 			if (folder.split(".")[folder.split(".").length - 1] == "js") {
-				files = files + 1
-				return fs.readFileSync(folder).toString().split("\n").length
+				var txt = fs.readFileSync(folder).toString()
+
+				return [txt.split("\n").length,txt.length,1]
 			}
 		}
 	}
-	return 0
+	return [0,0,0]
 }
 
 module.exports = {
@@ -35,14 +39,10 @@ module.exports = {
 			description: "A command that shows all the lines of code in js files, excluding node_modules.",
 			execute: async(c, m, a) => {
 
-				var lines = 0;
-				files = 0
-				for (var folder of fs.readdirSync(".")) {
-					lines = lines + thing("./" + folder)
-				}
+				var t = thing(".");
 				m.reply(new Discord.MessageEmbed()
 				.setColor("#23d160")
-				.setDescription(`:keyboard: I searched, and I found ${lines} lines of JavaScript over ${files} files (avg. ${Math.floor(lines / files)} lines per file)! (excluding those in node_modules)`));
+				.setDescription(`:keyboard: I searched, and I found:\n ${t[0]} lines of JavaScript over ${t[2]} files (avg. ${Math.floor(t[0] / t[2])} lines per file)!\n${t[1].toLocaleString('en')} total characters in all JavaScript files (for an average of ${Math.floor(t[1] / t[2]).toLocaleString('en')} characters per file). \n(excluding those in node_modules)`));
 			},
 		},
 	]
